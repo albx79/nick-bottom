be0b2ec04f41d2b64d6a30b001ebc3fbdc49b0e1
colomboalb <alberto.colombo@skytv.it>
2021-02-20 08:06:21 +0100

    initial commit

---
+++ src/test/java/it/albx79/nickbottom/NickBottomApplicationTests.java
@@ -0,0 +1,13 @@
+package it.albx79.nickbottom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NickBottomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
